621ce6bcb5f9be7148e8f10141260f70e511c49e
SAGAR SINHA <sagar0327@gmail.com>
2016-06-17 18:08:39 -0400

    Signup Epic Completed

--- SignupViewController.swift
+++ SignupViewController.swift
@@ -13,20 +13,51 @@ import ParseFacebookUtilsV4
 import FBSDKLoginKit
 import CoreData
 
-class SignupViewController:UIViewController, UITextFieldDelegate{
+class SignupViewController:UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     @IBOutlet weak var main_title: UILabel!
-    @IBOutlet weak var businessSwitch: UISwitch!
     @IBOutlet weak var interestLabel: UILabel!
-    
     @IBOutlet weak var friendshipLabel: UILabel!
     @IBOutlet weak var loveLable: UILabel!
     @IBOutlet weak var businessLabel: UILabel!
+    @IBOutlet weak var businessSwitch: UISwitch!
     @IBOutlet weak var friendshipSwitch: UISwitch!
     @IBOutlet weak var loveSwitch: UISwitch!
     @IBOutlet weak var profilePicture: UIImageView!
     @IBOutlet weak var nameTextField: UITextField!
     
+    // globally required as we do not want to re-create them everytime and for persistence
+    let imagePicker = UIImagePickerController()
     var editableName:String = ""
+    
+    
+    @IBAction func editImageTapped(sender: AnyObject) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+
+    }
+    //update the UIImageView once an image has been picked
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            profilePicture.image = pickedImage
+            profilePicture.layer.cornerRadius = profilePicture.frame.size.width/2
+            profilePicture.clipsToBounds = true
+            if let imageData = UIImagePNGRepresentation(pickedImage){
+            let localData = LocalData()
+            localData.setMainProfilePicture(imageData)
+            localData.synchronize()
+            }
+
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    // Begin Bridging Button Clicked
     @IBAction func beginBridgingTouched(sender: AnyObject) {
         if let _ = PFUser.currentUser() {
          PFUser.currentUser()?["name"] = editableName
@@ -36,6 +67,7 @@ class SignupViewController:UIViewController, UITextFieldDelegate{
         }
         
     }
+    // Switches tapped
     @IBAction func friendshipSwitchTapped(sender: AnyObject) {
         if friendshipSwitch.on{
             friendshipLabel.textColor = UIColor.blackColor()
@@ -65,6 +97,9 @@ class SignupViewController:UIViewController, UITextFieldDelegate{
 
     override func viewDidLoad() {
                 super.viewDidLoad()
+        imagePicker.delegate = self
+        nameTextField.delegate = self
+        
         let username = LocalData().getUsername()
         
         if let username = username {
@@ -76,7 +111,6 @@ class SignupViewController:UIViewController, UITextFieldDelegate{
             nameTextField.text = "A man has no name!"
         }
         main_title.attributedText = twoColoredString(editableName+"'s interests")
-        nameTextField.delegate = self
         nameTextField.hidden = true
         interestLabel.hidden = true
         main_title.userInteractionEnabled = true
@@ -101,24 +135,28 @@ class SignupViewController:UIViewController, UITextFieldDelegate{
         profilePicture.clipsToBounds = true
         
     }
+    // Utility functions
+    // generate a 2-colored string. Used for the title
     func twoColoredString(s:String)->NSMutableAttributedString{
         let mutableString = NSMutableAttributedString(string: s)
         mutableString.addAttribute(NSForegroundColorAttributeName, value: UIColor.blackColor(), range: NSRange(location:s.characters.count-12, length:12))
         return mutableString
         
     }
+    // Username label is tapped. Textfield should appear and replace the label
     func lblTapped(){
         main_title.hidden = true
         nameTextField.hidden = false
         interestLabel.hidden = false
         nameTextField.text = editableName
     }
+    // Tapped anywhere else on the main view. Textfield should be replaced by label
     func tappedOutside(){
         main_title.hidden = false
         nameTextField.hidden = true
         interestLabel.hidden = true
     }
-    
+    // User returns after editing
     func textFieldShouldReturn(userText: UITextField) -> Bool {
         userText.resignFirstResponder()
         nameTextField.hidden = true
